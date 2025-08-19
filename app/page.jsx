cd7a79e4f8ffe9b38daaed30e460343104968a2f
Anas Hadfi <anvshf@gmail.com>
2025-08-19 12:59:27 +0100

    first section of the website completed, with some errors to debug, such as the invisible navbar

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+export default function Home() {
+  return (
+    <>
+    <Navbar />
+    <Header/>
+    </>
+  );
+}
