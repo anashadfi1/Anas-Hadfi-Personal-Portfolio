513f5b6a34aabe7dbb21ffddea116ab5a9c283be
anashadfi1 <anvshf@gmail.com>
2026-02-12 23:12:41 +0100

    adding Work section to the portfolio

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,8 +1,39 @@
+import { workData } from '@/assets/assets'
 import React from 'react'
-
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
 const Work = () => {
   return (
-    <div>Work</div>
+    <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
+        <h4 className='text-center mb-2 text-lg font-Ovo'>Work</h4>
+        <h2 className='text-center text-5xl font-Ovo'>My professional projects</h2>
+        <p
+                className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+                    Welcome to my Data Portfolio! Explore a collection of projects showcasing my expertise in Data Science, analytics and engineering.
+                    </p>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 my-10'>
+            {
+                workData.map((project,index)=>(
+                <div key={index} 
+                className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
+                style={{backgroundImage:`url(${project.bgImage})`}}>
+                    <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 
+                        -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500
+                        group-hover:bottom'>
+                        <div>
+                            <h2>{project.title}</h2>
+                            <p>{project.description}</p>
+                        </div>
+                    <div>
+                        <Image src={assets.send_icon} alt='send-icon' className='w-5'/>
+                    </div>
+                    </div>
+                    
+
+                </div>))
+            }
+        </div>
+    </div>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/navbar";
 import Header from "./components/header";
 import About from "./components/about";
+import Work from "./components/Work";
 export default function Home() {
   return (
     <>
     <Navbar />
     <Header/>
     <About/>
+    <Work/>
     </>
   );
 }
