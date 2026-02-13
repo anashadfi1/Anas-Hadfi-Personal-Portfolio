7e9c1f37aa07a1b328521d3ccf51c8483fd98601
anashadfi1 <anvshf@gmail.com>
2026-02-13 02:42:52 +0100

    Adding the Contact-Me component

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -30,8 +30,7 @@ const Navbar = () => {
         >
             <li> <a className='font-Ovo' href="#top">Home</a></li>
             <li> <a className='font-Ovo' href="#about">About me</a></li>
-            <li> <a className='font-Ovo' href="#services">Services</a></li>
-            <li> <a className='font-Ovo' href="#wor">My work</a></li>
+            <li> <a className='font-Ovo' href="#work">My Work</a></li>
             <li> <a className='font-Ovo' href="#contact">Contact me</a></li>
         </ul>
         <div className='flex items-center gap-4'>
@@ -50,8 +49,7 @@ const Navbar = () => {
           </div>
           <li> <a className='font-Ovo' onClick={closeMenu} href="#top">Home</a></li>
             <li> <a className='font-Ovo' onClick={closeMenu} href="#about">About me</a></li>
-            <li> <a className='font-Ovo' onClick={closeMenu} href="#services">Services</a></li>
-            <li> <a className='font-Ovo' onClick={closeMenu} href="#wor">My work</a></li>
+            <li> <a className='font-Ovo' onClick={closeMenu} href="#wor">My Work</a></li>
             <li> <a className='font-Ovo' onClick={closeMenu} href="#contact">Contact me</a></li>
         </ul>
       </nav>
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar";
 import Header from "./components/header";
 import About from "./components/about";
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 export default function Home() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function Home() {
     <Header/>
     <About/>
     <Work/>
+    <Contact/>
     </>
   );
 }
